b30149c58866cfb1546a32f6996283eb368912c3
Jinwoo <zvzv9808@gmail.com>
2023-06-10 19:09:40 +0900

    4-1) Error Resolution with WebConfig addCorsMapping

---
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -0,0 +1,25 @@
+package com.app.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**") //어떤 url로 요청이 왔을 때 허용할 것인지 작성
+                .allowedOrigins("*", "http://localhost:8082") //어떤 Origin을 허용할 것인지
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                );
+    }
+}
