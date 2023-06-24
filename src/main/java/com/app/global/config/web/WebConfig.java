54cb3e8ca04596e5af76a903701025a7518a1f80
Jinwoo <zvzv9808@gmail.com>
2023-06-25 02:42:12 +0900

    9-12) Authentication Interceptor

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,18 @@
 package com.app.global.config.web;
 
+import com.app.global.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -23,5 +29,17 @@ public class WebConfig implements WebMvcConfigurer {
                 ).maxAge(3600); //Access-Control-Max-Age: 3600 으로 설정
         // 접근 제어 시간을 3600ms로 설정
         // default -> 1800
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1) // 인터셉터 실행 순서를 가장 최우선으로 설정
+                .addPathPatterns("/api/**") // 인증 인터셉터가 어떤 요청에 대해 작동할지 설정
+                .excludePathPatterns(
+                        "/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout",
+                        "/api/health"); // 인증 인터셉터가 작동하지 않을 특정 uri를 지정
     }
 }
