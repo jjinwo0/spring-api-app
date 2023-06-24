b900d2e23a7484fd8b8afaa4eaea53c80ac7b628
Jinwoo <zvzv9808@gmail.com>
2023-06-25 01:58:33 +0900

    9-11) Logout: update Member RefreshToken Expire Time

--- src/main/java/com/app/global/jwt/constant/TokenType.java
+++ src/main/java/com/app/global/jwt/constant/TokenType.java
@@ -3,4 +3,9 @@ package com.app.global.jwt.constant;
 public enum TokenType {
 
     ACCESS, REFRESH;
+
+    public static boolean isAccessToken(String tokenType){
+
+        return TokenType.ACCESS.name().equals(tokenType);
+    }
 }
